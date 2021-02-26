d2e2b53b2af8edba3d54947d0a5c83b6392f5504
Suvi <suvi.s.vaisanen@gmail.com>
2021-02-26 18:42:22 +0100

    Questions slide forms

--- EcoLabels/__init__.py
+++ EcoLabels/__init__.py
@@ -21,8 +21,13 @@ class Group(BaseGroup):
 
 
 class Player(BasePlayer):
-    Q1 = models.StringField(label="Question 1:")
-    Q2 = models.StringField(label="Question 2:")
+    Q1 = models.IntegerField(label="Question 1: How many trials are there?")
+    Q2 = models.BooleanField(label="Question 2: Are you a wizard?", choices= [
+    [1, 'Yes'],
+    [2, 'No'],
+    ])
+    Q3 = models.StringField(label="Question 3: Which object is shown in the ecolabel?", choices= ["leaf", "tree", "flower"])
+    Q4 = models.IntegerField(label = "Question 4: How well did you understand the       instructions? ", widget=widgets.RadioSelectHorizontal, choices=[1,2,3,4,5])
 
 
 # PAGES
@@ -31,7 +36,12 @@ class Introduction(Page):
 
 class Instructions(Page):
     form_model = 'player'
-    form_fields = ['Q1', 'Q2']
+    form_fields = ['Q1', 'Q2', 'Q3', 'Q4']
+
+
+    def error_message(player, values):
+        if (values['Q1'] != 50) or (values['Q2'] != 1) or (values['Q3'] != "leaf") or (values['Q4'] != 1):
+            return 'Error message'
 
 class Results(Page):
     pass
