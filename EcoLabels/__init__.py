dc14b4091f51d954dcbec6efd93210e6e93f988b
Suvi <suvi.s.vaisanen@gmail.com>
2021-05-02 19:00:13 +0200

    can't click next without giving answer

--- EcoLabels/__init__.py
+++ EcoLabels/__init__.py
@@ -27,13 +27,13 @@ class Player(BasePlayer):
     Q4 = models.IntegerField()
 
     # Variables for Demographics
-    D0 = models.StringField()
     D1 = models.StringField()
     D2 = models.StringField()
     D3 = models.StringField()
     D4 = models.StringField()
     D5 = models.StringField()
     D6 = models.StringField()
+    D7 = models.StringField()
 
     # variables for Questionnaire
     QT1 = models.StringField()
@@ -43,6 +43,26 @@ class Player(BasePlayer):
     QT5 = models.StringField()
     QT6 = models.StringField()
     QT7 = models.StringField()
+    QT8 = models.StringField()
+    QT9 = models.StringField()
+    QT10 = models.StringField()
+    QT11= models.StringField()
+    QT12= models.StringField()
+    QT13 = models.StringField()
+    QT14 = models.StringField()
+    QT15 = models.StringField()
+    QT16= models.StringField()
+    QT17= models.StringField()
+    QT18= models.StringField()
+    QT19= models.StringField()
+    QT20= models.StringField()
+    QT21= models.StringField()
+    QT22 = models.StringField()
+    QT23 = models.StringField()
+    QT24= models.StringField()
+    QT25 = models.StringField()
+    QT26 = models.StringField()
+    QT27 = models.StringField()
 
 # PAGES
 class Introduction(Page):
@@ -54,11 +74,11 @@ class Instructions(Page):
 
 class Demographics(Page):
     form_model = 'player'
-    form_fields = ['D0', 'D1', 'D2', 'D3', 'D4', 'D5', 'D6']
+    form_fields = ['D1', 'D2', 'D3', 'D4', 'D5', 'D6', 'D7']
 
 class Questionnaire(Page):
     form_model = 'player'
-    form_fields = ['QT1', 'QT2', 'QT3','QT4', 'QT5', 'QT6', 'QT7']
+    form_fields = ['QT1', 'QT2', 'QT3','QT4', 'QT5', 'QT6', 'QT7','QT8', 'QT9', 'QT10', 'QT11', 'QT12', 'QT13', 'QT14', 'QT15','QT16', 'QT17', 'QT18','QT19', 'QT20', 'QT21', 'QT22','QT23', 'QT24', 'QT25','QT26', 'QT27']
 
     def is_displayed(self):
         return True
@@ -67,6 +87,6 @@ class Results(Page):
     pass
 
 
-page_sequence = [Questionnaire, Demographics, Introduction, Instructions, Results]
+page_sequence = [Demographics, Questionnaire, Introduction, Instructions, Results]
 
 
