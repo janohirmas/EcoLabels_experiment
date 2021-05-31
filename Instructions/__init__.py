290be0bcd4249f796cae33d688f1e19dd511b5d6
Suvi <suvi.s.vaisanen@gmail.com>
2021-05-31 20:50:51 +0200

    consent form added

--- Instructions/__init__.py
+++ Instructions/__init__.py
@@ -19,20 +19,26 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
 
-    # variables for instructions
+    # Terms & conditions
+    TC = models.StringField()
+
+    # Variables for instructions
     Q1 = models.IntegerField()
     Q2 = models.StringField()
     Q3 = models.StringField()
-
 
 # PAGES
 class Introduction(Page):
     pass
 
+class ConsentForm(Page):
+    form_model = 'player'
+    form_fields = ['TC']
+
 class Instructions(Page):
     form_model = 'player'
     form_fields = ['Q1', 'Q2', 'Q3']
 
-page_sequence = [Introduction, Instructions]
+page_sequence = [Introduction, ConsentForm, Instructions]
 
 
